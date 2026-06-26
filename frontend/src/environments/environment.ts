6be41976b8aee92ec75d856cda1d3b40bff8d88f
milimoe <mili@wrss.org>
2026-06-26 21:02:17 +0800

    chore: 还原environment配置为本地开发模式

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
-  production: true,
-  // Capacitor 打包时改为你的服务器地址
-  // 例如: 'http://192.168.5.14:5185' 或 'https://your-domain.com'
-  apiBase: 'http://47.97.119.185:5111'
+  production: false,
+  apiBase: ''
 };
